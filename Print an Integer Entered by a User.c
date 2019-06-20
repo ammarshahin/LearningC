265296deca4bdb244e6db4b0ad2fda9c6c8b8e33
ammar shahin <30987238+ammarshahin@users.noreply.github.com>
2019-06-20 11:48:45 +0200

    Create Print an Integer Entered by a User.c

---
+++ Print an Integer Entered by a User.c
@@ -0,0 +1,12 @@
+/* The program is a C Program to Print an Integer Entered by a User
+   Created by : ammar shahin */
+#include <stdio.h>  /* include the header file containing function used */
+
+int main()
+{ /* the program starts here */
+  int x;  /* declare a variable */
+	printf("Enter the value of x : "); /* print out a sentence */
+	scanf("%d",&x);                    /* get inputs from users */
+	printf("The value of x is: %d",x); /* print out the value of the variable */
+	return 0;
+} /* the program Ends here */
