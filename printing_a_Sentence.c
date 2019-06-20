45e1924e0b181e882f91a88342bd5fa694daa906
ammar shahin <30987238+ammarshahin@users.noreply.github.com>
2019-06-20 11:42:48 +0200

    Create printing_a_Sentence.c

---
+++ printing_a_Sentence.c
@@ -0,0 +1,9 @@
+/* The program is used for printing out a Sentence
+   Created by : ammar shahin */
+#include <stdio.h>  /* include the header file containing function used */
+
+int main()
+{ /* the program starts here */
+	printf("Hallo alle zusammen!. Ich spreche Deutsch\n"); /* to print out a sentence */
+	return 0;
+} /* the program Ends here */
