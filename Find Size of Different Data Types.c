de626368ef37cc418b767ded244550c4f6cfc06e
ammar shahin <30987238+ammarshahin@users.noreply.github.com>
2019-06-20 11:53:15 +0200

    Create Find Size of Different Data Types.c

---
+++ Find Size of Different Data Types.c
@@ -0,0 +1,12 @@
+/* The program is a C Program to Find Size of int, float, double and char of Your System
+   Created by : ammar shahin */
+#include <stdio.h>  /* include the header file containing function used */
+
+int main()
+{ /* the program starts here */
+	printf("the size of int in bytes is: %d byte\n",sizeof(int));
+	printf("the size of float in bytes is: %d byte\n",sizeof(float));
+	printf("the size of double in bytes is: %d byte\n",sizeof(double));
+	printf("the size of char in bytes is: %d byte\n",sizeof(char));
+	return 0;
+} /* the program Ends here */
